f1cbd202ea76a1c92db2363120fa8c1a2cce4868
Dhwani236 <dhwani2336@gmail.com>
2025-08-28 16:34:30 +0530

    Add files via upload

---
+++ postlabs/postlab1.py
@@ -0,0 +1,15 @@
+#a
+for i in range(0,5):
+ print("Welcome to Python")
+
+#b
+print("a\t a^2\t a^3")
+for i in range(1, 5):
+    print(f"{i}\t {i**2}\t {i**3}")
+
+#c
+numerator=9.5*4.5-2.5*3
+denominator=42.5-3.5
+result=numerator/denominator
+print(result)
+
